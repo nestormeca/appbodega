ded79302a299d51a12cb4f26171dff35dfd75b98
nestormeca <nestor.m3@gmail.com>
2022-04-07 22:00:37 -0400

    prime commit

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import { Container, AppBar, Typography } from "@mui/material";
+import React from "react";
+import useStyles from "../styles";
+
+export const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        {/* <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        /> */}
+      </AppBar>
+    </Container>
+  );
+};
